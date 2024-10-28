0812d7b01d4f5d5ad727fe674b4ec67556467be5
Sukanta Mukherjee <sukantomukherjee802gmail.com>
2024-10-28 17:28:18 +0600

    variable updated

--- batchRequestSend.mjs
+++ batchRequestSend.mjs
@@ -49,7 +49,7 @@ const validationHeader = {
         'Connection': 'keep-alive',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0',
     },
-   timeout: 20000
+//    timeout: 20000
 };
 
 // Setting extracted cookies in header format
